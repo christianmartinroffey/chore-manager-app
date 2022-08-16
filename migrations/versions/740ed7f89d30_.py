3916c65e46f2953a75fb803e729ec81aff0f55c4
christianmartinroffey <c.martinroffey@gmail.com>
2022-08-16 15:32:31 +0000

    reset db

--- migrations/versions/740ed7f89d30_.py
+++ migrations/versions/740ed7f89d30_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 51001362c266
+Revision ID: 740ed7f89d30
 Revises: 
-Create Date: 2022-08-13 09:57:08.045591
+Create Date: 2022-08-16 15:32:15.057747
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '51001362c266'
+revision = '740ed7f89d30'
 down_revision = None
 branch_labels = None
 depends_on = None
